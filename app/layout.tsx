0cea0a164185a3a1eb648164fbac8d6e13823a68
Ajaysharma <akpancholi8@gmail.com>
2025-07-02 12:39:00 +0530

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   title: "AdHabits - Professional Meta Ads for Affiliate Marketers",
   description:
     "Let us run your Meta ads and get you more leads professionally. 3+ years experience, 250+ affiliates served.",
-    generator: 'v0.dev'
+  generator: 'v0.dev'
 }
 
 export default function RootLayout({
@@ -19,6 +19,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
+      <head>
+        <script>
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod ?
+            n.callMethod.apply(n, arguments) : n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '905585234905857');
+          fbq('track', 'PageView');
+        </script>
+        <noscript><img height="1" width="1" style="display:none"
+          src="https://www.facebook.com/tr?id=905585234905857&ev=PageView&noscript=1"
+        /></noscript>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
